17b3dc459f9916cac337dac869dca447f48202c5
paulzfm <zhufengminpaul@163.com>
2015-08-05 18:01:59 +0800

    update naive version

--- knn_naive.cu
+++ knn_naive.cu
@@ -3,7 +3,7 @@
 #include <math.h>
 
 #define INF 1073741824
-#define BLOCK_SZ 32
+#define BLOCK_SZ 16
 
 int m; // nodes
 int n; // dimensions
@@ -25,8 +25,7 @@ int* load(const char *input)
     int *data = (int*)malloc(sizeof(int) * m * n);
 
     // load data
-    int i;
-    for (i = 0; i < m * n; i++) {
+    for (int i = 0; i < m * n; i++) {
         fscanf(file, "%d", data + i);
     }
 
@@ -36,19 +35,20 @@ int* load(const char *input)
 
 __global__ void distances(int *data, int *dis, int m, int n)
 {
-    int idx = blockDim.x * blockIdx.x + threadIdx.x;
-    if (idx >= m * m) return;
-    int i = idx / m;
-    int j = idx % m;
-    int tmp = 0;
-    if (i < j) {
-        for (int l = 0; l < n; l++) { // for each dimension
-            tmp += (data[i * n + l] - data[j * n + l])
-                * (data[i * n + l] - data[j * n + l]);
-        }
-        dis[i * m + j] = dis[j * m + i] = tmp;
-    } else if (i == j) {
+    int i = blockDim.x * blockIdx.x + threadIdx.x;
+    int j = blockDim.y * blockIdx.y + threadIdx.y;
+    if (i > j || i >= m || j >= m) return;
+
+    if (i == j) {
         dis[i * m + i] = INF;
+    } else {
+        int tmp1;
+        int tmp2 = 0;
+        for (int l = 0; l < n; l++) { // for each dimension
+            tmp1 = data[i * n + l] - data[j * n + l];
+            tmp2 += tmp1 * tmp1;
+        }
+        dis[i * m + j] = dis[j * m + i] = tmp2;
     }
 }
 
@@ -73,30 +73,34 @@ __global__ void sort(int *dis, int *result, int m, int k)
 void knn(int *data, int *result)
 {
     int *d_data, *d_result, *d_dis;
-    int block1 = ceil(m * m / (double)BLOCK_SZ);
-    int block2 = ceil(m / (double)BLOCK_SZ);
+    int block = ceil(m / (double)BLOCK_SZ);
     float timer;
 
     cudaEvent_t start, stop;
     cudaEventCreate(&start);
     cudaEventCreate(&stop);
-    cudaEventRecord(start);
 
     cudaMalloc((void**)&d_data, sizeof(int) * m * n);
     cudaMalloc((void**)&d_result, sizeof(int) * m * k);
     cudaMalloc((void**)&d_dis, sizeof(int) * m * m);
     cudaMemcpy(d_data, data, sizeof(int) * m * n, cudaMemcpyHostToDevice);
 
-    distances<<<block1, BLOCK_SZ>>>(d_data, d_dis, m, n);
-    cudaStreamSynchronize(0);
-    sort<<<block2, BLOCK_SZ>>>(d_dis, d_result, m, k);
-    cudaMemcpy(result, d_result, sizeof(int) * m * k, cudaMemcpyDeviceToHost);
+    cudaEventRecord(start);
+    distances<<<dim3(block, block, 1), dim3(BLOCK_SZ, BLOCK_SZ, 1)>>>(d_data, d_dis, m, n);
+    cudaEventRecord(stop);
+    cudaEventSynchronize(stop);
+    cudaEventElapsedTime(&timer1, start, stop);
 
+    cudaEventRecord(start);
+    sort<<<block2, BLOCK_SZ>>>(d_dis, d_result, m, k);
     cudaEventRecord(stop);
     cudaEventSynchronize(stop);
     cudaEventElapsedTime(&timer, start, stop);
 
-    printf("Time elapsed: %.4lf ms\n", timer);
+    cudaMemcpy(result, d_result, sizeof(int) * m * k, cudaMemcpyDeviceToHost);
+
+    fprintf(stderr, "distance: %.4lf ms\n", timer1);
+    fprintf(stderr, "sort: %.4lf ms\n", timer2);
 
     cudaFree(d_data);
     cudaFree(d_result);
@@ -122,10 +126,10 @@ int main(int argc, char **argv)
 
     // output
     for (int i = 0; i < m; i++) {
-        for (int j = 0; j < k - 1; j++) {
+        for (int j = 0; j < k; j++) {
             printf("%d ", result[i * k + j]);
         }
-        printf("%d\n", result[i * k + k - 1]);
+        printf("\n");
     }
 
     free(data);
